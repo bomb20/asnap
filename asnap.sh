04b44fe04e5ad21568f4f4cc396b62cd3a002c61
Vincent Truchseß <42@arkme.de>
2016-04-05 03:20:20 +0200

    Made structureized Comments

--- asnap.sh
+++ asnap.sh
@@ -1,1 +1,29 @@
 #!/bin/sh
+
+#asnap provides incremental rotating snapsots of folders using rsync
+
+##Config Section
+
+
+#Get current Timestamp
+
+
+#check for jearly rotation
+
+#check for monthly rotation
+
+#check for weekly rotation
+
+#check for daily rotation
+
+#check for hourly rotation
+
+#determine if last snapshot is present
+
+#perform hourly snapshot
+if ($LastPresent == true do){
+	cp -al $SnapshotDir/$LastSnapshot/ $SnapshotDir/$TimeStamp/;
+	rsync -valuz --delete --include-from="./includes" --exclude-from="excludes" $BackupDir/ $SnapshotDir/$TimeStamp/;
+}
+
+
